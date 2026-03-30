120f04ae34d3e21419f48580073656490e7a5287
chriskilgi <christoph.kilgenstein@gmx.de>
2026-03-30 21:27:28 +0200

    Enhance CTIMER class to support optional timer callback function in constructor and update related member variable

--- include/tim.h
+++ include/tim.h
@@ -1,45 +1,52 @@
 #pragma once
 #include <Arduino.h>
 
+// Abstraction for ESP32 hardware timers with callback functions and auto-reload functionality.
 class CTIMER {
 public:
-    CTIMER(int id, uint64_t interval_us, bool autoReload = true);
+    CTIMER(int id, uint64_t interval_us, void (*pTimerCallbackFunction)() = nullptr, bool autoReload = true);
     void start();
     void stop();
 
     virtual void onTimer();   // wird bei jedem Interrupt aufgerufen
 
+    // Method to set or change the timer callback function,
+    // allowing dynamic assignment of the callback after timer creation
     void setTimerCallbackFunction(void (*pTimerCallbackFunction)()) {
-        this->TimerCallback = pTimerCallbackFunction;
+        this->pTimerCallbackFunction = pTimerCallbackFunction;
     }
 
 protected:
-    void (*TimerCallback)() = nullptr; // Zeiger auf die Timer-Callback-Funktion, die im Interrupt aufgerufen wird
+    // Pointer to the timer callback function, which will be called in the interrupt service routine
+    void (*pTimerCallbackFunction)() = nullptr; // Initially set to nullptr, can be assigned a valid function pointer by the user
+
 
 private:
     hw_timer_t* hwTimer;
-    int id;
-    int groupId;
-    int timerId;
+    int id; // Timer ID (0-3, where 0 and 1 belong to group 0, and 2 and 3 belong to group 1)
+    int groupId; // Timer group ID (0 or 1)
+    int timerId; // Timer ID within the group (0 or 1)
 
-    // Instanz-Tabelle: 4 Timer → 4 Einträge
+    // Table of instances to allow static ISR wrappers to call the correct instance's onTimer() method
     static CTIMER* instances[4];
 
-    // Statische ISR-Wrapper
+    // Static ISR wrappers that call the onTimer() method of the correct instance based on the timer ID
     static void IRAM_ATTR isr0();
     static void IRAM_ATTR isr1();
     static void IRAM_ATTR isr2();
     static void IRAM_ATTR isr3();
 };
 
+// Specialized timer class that calls a user-defined callback function on each timer interrupt
 class CMCBTIMER : public CTIMER {
 public:
-    CMCBTIMER(int id, uint64_t interval_us)
-        : CTIMER(id, interval_us, true) {}
+    CMCBTIMER(int id, uint64_t interval_us, void (*pTimerCallbackFunction)() = nullptr)
+        : CTIMER(id, interval_us, pTimerCallbackFunction, true) {}
 
+    // Override the onTimer method to call the user-defined callback function
     void onTimer() override {
-        if (this->TimerCallback != nullptr) {
-            this->TimerCallback(); // Rufe die Callback-Funktion auf, wenn sie gesetzt ist
+        if (this->pTimerCallbackFunction != nullptr) {
+            this->pTimerCallbackFunction(); // Call the user-defined callback function if it is set
         }  
     }
 };
--- src/tim.cpp
+++ src/tim.cpp
@@ -1,22 +1,22 @@
 #include <tim.h>
 
-// Instanz-Tabelle definieren
+// Table of instances to allow static ISR wrappers to call the correct instance's onTimer() method
 CTIMER* CTIMER::instances[4] = { nullptr, nullptr, nullptr, nullptr };
 
-CTIMER::CTIMER(int id, uint64_t interval_us, bool autoReload)
-    : id(id)
+CTIMER::CTIMER(int id, uint64_t interval_us, void (*pTimerCallbackFunc)(), bool autoReload)
+    : id(id), pTimerCallbackFunction(pTimerCallbackFunc)
 {
-    // ID → group + timer umrechnen
+    // ID → group + timer mapping
     groupId = id / 2;     // 0 oder 1
     timerId = id % 2;     // 0 oder 1
 
-    // Instanz registrieren
+    // Storing the instance in the static table for ISR access
     instances[id] = this;
 
-    // Hardware-Timer erzeugen
+    // Get the hardware timer instance
     hwTimer = timerBegin(timerId, 80, true);  // 1 µs Ticks
 
-    // ISR verbinden
+    // Attach the appropriate ISR based on the timer ID
     switch (id) {
         case 0: timerAttachInterrupt(hwTimer, &CTIMER::isr0, true); break;
         case 1: timerAttachInterrupt(hwTimer, &CTIMER::isr1, true); break;
@@ -24,7 +24,7 @@ CTIMER::CTIMER(int id, uint64_t interval_us, bool autoReload)
         case 3: timerAttachInterrupt(hwTimer, &CTIMER::isr3, true); break;
     }
 
-    // Alarm konfigurieren
+    // Configure the timer alarm with the specified interval and auto-reload setting
     timerAlarmWrite(hwTimer, interval_us, autoReload);
 }
 
@@ -37,11 +37,11 @@ void CTIMER::stop() {
 }
 
 void CTIMER::onTimer() {
-    // Standard: nichts tun
+    // This method can be overridden by derived classes to implement specific behavior on timer interrupts
 }
 
 // ---------------- ISR-WRAPPER ----------------
-
+// Static ISR wrappers that call the onTimer() method of the correct instance based on the timer ID
 void IRAM_ATTR CTIMER::isr0() { if (instances[0]) instances[0]->onTimer(); }
 void IRAM_ATTR CTIMER::isr1() { if (instances[1]) instances[1]->onTimer(); }
 void IRAM_ATTR CTIMER::isr2() { if (instances[2]) instances[2]->onTimer(); }
