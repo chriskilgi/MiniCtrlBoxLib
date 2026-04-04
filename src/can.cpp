706b088df9aaa181719273f5dc6a6d6801f82f49
chriskilgi <christoph.kilgenstein@gmx.de>
2026-04-04 08:52:25 +0200

    Bump version to 3.0.1 in library.json and glo.h, and ensure CAN bus termination is disabled at startup in CCanBus constructor

--- src/can.cpp
+++ src/can.cpp
@@ -4,6 +4,7 @@
 CCanBus::CCanBus(gpio_num_t txPin, gpio_num_t rxPin, uint32_t bitrate)
     : txPin(txPin), rxPin(rxPin), bitrate(bitrate), running(false)
 {
+    setCANTermination(false); // Ensure CAN bus termination is disabled at startup
 }
 
 CCanBus::~CCanBus() {
