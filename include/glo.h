122661566536946bac3e95e814d5b4e46459e251
chriskilgi <christoph.kilgenstein@gmx.de>
2026-04-11 17:36:38 +0200

    Header of the library back to include path!

--- include/glo.h
+++ include/glo.h
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 namespace nspMiniCtrlBox {
-#define MINICTRLBOX_SW_VERSION "5.0.5"
+#define MINICTRLBOX_SW_VERSION "5.0.6"
 #define MINICTRLBOX_HW_VERSION "3.0.1"
 
 bool gloIsI2CDevicePresent(uint8_t ui8DeviceAddress);
