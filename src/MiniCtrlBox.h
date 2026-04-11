65564d4008cfe28aaefebc676b8ceaaf00635864
chriskilgi <christoph.kilgenstein@gmx.de>
2026-04-11 17:12:10 +0200

    Refactor MiniCtrlBox Library Moving all include file from include path to src path

--- src/MiniCtrlBox.h
+++ src/MiniCtrlBox.h
@@ -1,13 +1,13 @@
 #pragma once
 
-#include <../include/btn.h>
-#include <../include/eep.h>
-#include <../include/glo.h>
-#include <../include/mcp.h>
-#include <../include/can.h>
-#include <../include/dsp.h>
-#include <../include/pca.h>
-#include <../include/tim.h>
-#include <../include/asbActors.h>
-#include <../include/asbSensors.h>
+#include <btn.h>
+#include <eep.h>
+#include <glo.h>
+#include <mcp.h>
+#include <can.h>
+#include <dsp.h>
+#include <pca.h>
+#include <tim.h>
+#include <asbActors.h>
+#include <asbSensors.h>
 
