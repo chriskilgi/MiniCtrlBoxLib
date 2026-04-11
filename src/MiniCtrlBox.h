1003c48575e254d7253e2029e60fc2b08673d30f
chriskilgi <christoph.kilgenstein@gmx.de>
2026-04-11 16:30:55 +0200

    Update version to 5.0.3 in glo.h and library.json; add MiniCtrlBox.h header file to src path

---
+++ src/MiniCtrlBox.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <../include/btn.h>
+#include <../include/eep.h>
+#include <../include/glo.h>
+#include <../include/mcp.h>
+#include <../include/can.h>
+#include <../include/dsp.h>
+#include <../include/pca.h>
+#include <../include/tim.h>
+#include <../include/asbActors.h>
+#include <../include/asbSensors.h>
+
