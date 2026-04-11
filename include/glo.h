55176b90a5b8fe2203549c057b6948c727e6eee0
chriskilgi <christoph.kilgenstein@gmx.de>
2026-04-11 21:28:31 +0200

    Update version to 5.0.11 in glo.h and library.json

--- include/glo.h
+++ include/glo.h
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 namespace nspMiniCtrlBox {
-#define MINICTRLBOX_SW_VERSION "5.0.10"
+#define MINICTRLBOX_SW_VERSION "5.0.11"
 #define MINICTRLBOX_HW_VERSION "3.0.1"
 
 bool gloIsI2CDevicePresent(uint8_t ui8DeviceAddress);
