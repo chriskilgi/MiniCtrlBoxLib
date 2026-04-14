a061d60e27781e1a84b92dd0ec92e8b290bbef9d
chriskilgi <christoph.kilgenstein@gmx.de>
2026-04-14 22:45:37 +0200

    Update software version to 6.0.0 in glo.h and library.json Merge-Commit: interface of CEeprom is changed

--- include/glo.h
+++ include/glo.h
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 namespace nspMiniCtrlBox {
-#define MINICTRLBOX_SW_VERSION "5.1.0"
+#define MINICTRLBOX_SW_VERSION "6.0.0"
 #define MINICTRLBOX_HW_VERSION "3.0.1"
 
 bool gloIsI2CDevicePresent(uint8_t address, uint8_t retries = 5, uint16_t delayMs = 2);
